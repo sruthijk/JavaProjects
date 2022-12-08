946d874284db05d67d82eb3b34b205ae35e12d70
sruthijk <114891803+sruthijk@users.noreply.github.com>
2022-12-08 00:51:56 -0500

    Update HelloWorld.java

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("HELLO WORLD");
 		
 		
 		
